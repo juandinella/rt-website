94c989486df009200835b14ad2c34616d68157e2
Miguel Palau Zarza <mpalauzarza@gmail.com>
2018-03-27 20:04:11 -0600

    slider buttons

--- src/assets/js/home/problem.js
+++ src/assets/js/home/problem.js
@@ -1,18 +1,38 @@
 import { tns as tinySlider } from 'tiny-slider/src/tiny-slider.module'
-import { home } from 'styles'
+import { home, components } from 'styles'
+import moduleSelector from '../tools/moduleSelector'
 
-const problem = () =>
-  tinySlider({
+const problem = () => {
+  const slider = tinySlider({
     container: `.${home.problem.problems}`,
     nav: false,
     controls: false,
     mouseDrag: true,
     autoHeight: true,
+    loop: false,
     responsive: {
       1024: {
         disable: true
       }
     }
   })
+
+  const prevBtn = moduleSelector(components.controls.prev)
+  const nextBtn = moduleSelector(components.controls.next)
+  const current = moduleSelector(components.controls.current)
+
+  prevBtn.addEventListener('click', event => {
+    slider.goTo('prev')
+  })
+
+  nextBtn.addEventListener('click', event => {
+    slider.goTo('next')
+  })
+
+  slider.events.on('indexChanged', info => {
+    const { index } = info
+    current.textContent = index + 1
+  })
+}
 
 export default problem
