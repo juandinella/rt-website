dfc1eaeaf153221119c1f709b757a47909bf0ffa
Miguel Palau Zarza <mpalauzarza@gmail.com>
2018-03-30 16:29:01 -0600

    lazy load all the images

--- gulp/common/markup.js
+++ gulp/common/markup.js
@@ -1,23 +1,9 @@
 const gulp = require('gulp')
 const config = require('../config')
 const production = config.production
-const fs = require('fs')
 const pug = require('@pixel2html/pipes').pug
-const path = require('path')
-const cheerio = require('cheerio')
-const { cwd } = require('process')
 
-const icon = (name, attributes) => {
-  const file = path.join(cwd(), `src/assets/icons/${name}.svg`)
-  const icn = fs.readFileSync(file, 'utf-8')
-  const $ = cheerio.load(icn)
-  const svg = $('svg')
-  if (attributes) {
-    const arr = Object.keys(attributes)
-    arr.forEach(attr => svg.attr(attr, attributes[attr]))
-  }
-  return $('body').html()
-}
+const { blur, icon } = require('./pugHelpers')
 
 gulp.task('markup', () =>
   gulp.src(config.directories.src.markup + '/*.pug')
@@ -26,7 +12,8 @@ gulp.task('markup', () =>
         basedir: config.directories.src.markup,
         locals: {
           icon,
-          production
+          production,
+          blur
         }
       },
       cssModules: `./${config.directories.src.cssModules}`,
--- src/assets/js/general/general.js
+++ src/assets/js/general/general.js
@@ -1,8 +1,11 @@
 import asyncFonts from '../tools/async-fonts'
 // import gtm from './gtm'
 import smoothScroll from './smooth-scroll'
+import lazyImages from './lazyImages'
 
 const general = () => {
+  lazyImages()
+
   asyncFonts({
     href: 'https://fonts.googleapis.com/css?family=Lato:700,900|Source+Sans+Pro:400,400i,600,700,900',
     observer: 'Source Sans Pro'
