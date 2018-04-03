09cea0303e701022308eb03a02a95c62419f3bbd
Miguel Palau Zarza <mpalauzarza@gmail.com>
2018-04-03 13:38:56 -0500

    red line situation crustacean

--- src/assets/js/home/problem.js
+++ src/assets/js/home/problem.js
@@ -3,8 +3,9 @@ import { home, components } from 'styles'
 import moduleSelector from '../tools/moduleSelector'
 
 const problem = () => {
+  const problems = moduleSelector(home.problem.problems)
   const slider = tinySlider({
-    container: `.${home.problem.problems}`,
+    container: problems,
     nav: false,
     controls: false,
     mouseDrag: true,
@@ -20,6 +21,7 @@ const problem = () => {
   const prevBtn = moduleSelector(components.controls.prev)
   const nextBtn = moduleSelector(components.controls.next)
   const current = moduleSelector(components.controls.current)
+  const line = moduleSelector(home.problem.red)
 
   prevBtn.addEventListener('click', event => {
     slider.goTo('prev')
@@ -31,7 +33,12 @@ const problem = () => {
 
   slider.events.on('indexChanged', info => {
     const { index } = info
-    current.textContent = index + 1
+    const humanIndex = index + 1
+    const total = problems.childElementCount
+    current.textContent = humanIndex
+
+    const percentage = (humanIndex / total) * 100
+    line.setAttribute('width', percentage)
   })
 }
 
