be2be8f7312cbb63a0df8fea8e46277772b47f1f
Miguel Palau Zarza <mpalauzarza@gmail.com>
2018-04-29 20:22:52 -0500

    banner plugin now works

--- webpack/commonPlugins.js
+++ webpack/commonPlugins.js
@@ -4,25 +4,20 @@ let common = [
   // Do NOT import the BLOAT from moment.js
   // thanks create-react-app
   new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-  new webpack.BannerPlugin({
-    banner: `
-/*!********************************************************************
-*  ##      ##    ###    ########  ##    ## #### ##    ##  ######   ####
-*  ##  ##  ##   ## ##   ##     ## ###   ##  ##  ###   ## ##    ##  ####
-*  ##  ##  ##  ##   ##  ##     ## ####  ##  ##  ####  ## ##        ####
-*  ##  ##  ## ##     ## ########  ## ## ##  ##  ## ## ## ##   ####  ##
-*  ##  ##  ## ######### ##   ##   ##  ####  ##  ##  #### ##    ##
-*  ##  ##  ## ##     ## ##    ##  ##   ###  ##  ##   ### ##    ##  ####
-*  ###  ###  ##     ## ##     ## ##    ## #### ##    ##  ######   ####
-*
-*  Don't edit this file directly. Edit with Webpack.
-*  Then after you're done run npm run build
-*
-*  Scaffolded with @pixel2html/generator-frontend
-*  ********************************************************************/
-`,
-    raw: true
-  })
+  new webpack.BannerPlugin(`
+    ##      ##    ###    ########  ##    ## #### ##    ##  ######   ####
+    ##  ##  ##   ## ##   ##     ## ###   ##  ##  ###   ## ##    ##  ####
+    ##  ##  ##  ##   ##  ##     ## ####  ##  ##  ####  ## ##        ####
+    ##  ##  ## ##     ## ########  ## ## ##  ##  ## ## ## ##   ####  ##
+    ##  ##  ## ######### ##   ##   ##  ####  ##  ##  #### ##    ##
+    ##  ##  ## ##     ## ##    ##  ##   ###  ##  ##   ### ##    ##  ####
+    ###  ###  ##     ## ##     ## ##    ## #### ##    ##  ######   ####
+
+    Don't edit this file directly. Edit with Webpack.
+    Then after you're done run npm run build
+
+    Scaffolded with @pixel2html/generator-frontend
+  `)
 ]
 
 module.exports = common
